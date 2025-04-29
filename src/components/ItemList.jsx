4b9a1fd70d93c40f0d772bca1cff661f04bb2097
dhairya <dhairya.jangir.s73@kalvium.community>
2025-04-29 17:27:36 +0530

    Update ItemList.jsx

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,18 @@
+// ItemList.jsx
 import Item from "./Item";
 
-const ItemList = ({ items }) => {
-    // your code here
-    return (
-        <>
-            {items.map((item) => (
-                <Item key={item.id} item={item} />
-            ))}
-        </>
-    );
+const ItemList = ({ items, onDelete }) => {
+  return (
+    <div>
+      {items.length > 0 ? (
+        items.map((item) => (
+          <Item key={item.id} item={item} onDelete={onDelete} />
+        ))
+      ) : (
+        <p>No items found.</p>
+      )}
+    </div>
+  );
 };
 
 export default ItemList;
